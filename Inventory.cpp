08d5abb9bb2328997ea2c3acb3e6da752e0eaabb
doyeon113 <46617177+doyeon113@users.noreply.github.com>
2019-02-11 14:17:30 +0900

    아이템 클래스 다량 추가

---
+++ Inventory.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "Inventory.h"
+
+
+Inventory::Inventory()
+{
+}
+
+
+Inventory::~Inventory()
+{
+}
---
+++ Inventory.h
@@ -0,0 +1,8 @@
+#pragma once
+class Inventory
+{
+public:
+	Inventory();
+	~Inventory();
+};
+
