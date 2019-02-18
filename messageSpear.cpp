c475a93c06f0be7fd2457b368e7e0dd94e5aecf5
vebitas <46617166+vebitas@users.noreply.github.com>
2019-02-18 19:29:37 +0900

    플레이어 ui 던전씬에만 보이게

--- messageSpear.cpp
+++ messageSpear.cpp
@@ -38,24 +38,48 @@ void messageSpear::render()
 
 	if (KEYMANAGER->isStayKeyDown('V'))
 	{
+		IMAGEMANAGER->findImage("messageSpear")->expandRender(getMemDC(), 0, GAMESIZEY / 2 + 80, 0, 0, 1.7, 1.7);
+
+
 		HFONT font;
 		HFONT oldFont;
-		font = CreateFont(30, 20, 0, 0, 0, 0, 0, 0, HANGUL_CHARSET, 0, 0, 0, 0, "Sam3KRFont");
+		RECT rc;
+		rc = RectMake(0, 550, GAMESIZEX - 200, 400);
+		font = CreateFont(40, 25, 0, 0, 0, 0, 0, 0, HANGUL_CHARSET, 0, 0, 0, 0, "Sam3KRFont");
 		oldFont = (HFONT)SelectObject(getMemDC(), font);
 		vStr = TXTDATA->txtLoad("messageFile/할아버지.txt");
-		TextOut(getMemDC(), 100, 100, vStr[0].c_str(), strlen(vStr[0].c_str()));
+		//TextOut(getMemDC(), 80, 500, vStr[0].c_str(), strlen(vStr[0].c_str()));
+		//TextOut(getMemDC(), 80, 550, vStr[1].c_str(), strlen(vStr[1].c_str()));
+		//DrawText(getMemDC(), &vStr[0].c_str,strlen(vStr[0].c_str), &rc, DT_CENTER|DT_WORDBREAK);
 		SelectObject(getMemDC(), oldFont);
 		DeleteObject(font);
 	}
+
+	//HFONT font1, oldFont1;
+	//RECT rcText = RectMake(300, 200, 400, 100);
+	//
+	//font1 = CreateFont(100, 0, 0, 0, 300, 0, 0, 0,
+	//	DEFAULT_CHARSET, OUT_STRING_PRECIS, CLIP_DEFAULT_PRECIS,
+	//	PROOF_QUALITY, DEFAULT_PITCH | FF_SWISS, TEXT("Edwardian Script ITC"));
+	//oldFont1 = (HFONT)SelectObject(getMemDC(), font1);
+	//DrawText(getMemDC(), TEXT("Modern Times"), strlen("Modern Times"), &rcText, DT_CENTER | DT_VCENTER | DT_SINGLELINE);
+	//SelectObject(getMemDC(), oldFont1);
+	//DeleteObject(font1);
+	//SetBkMode(getMemDC(), TRANSPARENT);
 }
 
 void messageSpear::grandfaMessage()
 {
 	string message[2];
+	
 	vector<string> vStr;
 
-	message[0] = "대사";
-	message[1] = "";
+	message[0] = "잭";
+	//message[1] = "안녕하세요 \n 사랑해요";
+	message[1] = "안녕하세요\nㅋㅋㅋ";
+	//message[1][message[1].size()] = 10;
+	//message[1].append((char*)10);
+	//message[1][message[1].size()] = NULL;
 
 	vStr.push_back(message[0]);
 	vStr.push_back(message[1]);
